abe58d4a628183240d969d3c652c8b3702916a75
Aryanpachhai <aryanpachhai803@gmail.com>
2026-01-30 18:54:13 +0545

    Transformative Image

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ navbarToggle.addEventListener('click', ()=>{
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
 })
+
+
+const img = document.querySelector('.two-image img');
+
+let scale = 1;
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  scale = 1 + scrollY / 1000; 
+  img.style.transform = `scale(${scale})`;
+});
+
+
